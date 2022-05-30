a6f62a558f6d098dd45ff3df932fd52f3865a32c
vegatarianSpam23 <simonswu23@gmail.com>
2022-05-30 13:28:39 -0700

    finished basic visualization

--- 163_Simon.py
+++ 163_Simon.py
@@ -38,12 +38,12 @@ def main():
     #all_gender = pd.concat([t_men, t_women, c_men, c_women, nb]).reset_index(drop=True)
     #print(all_gender)
 
-    fig, ax = plt.subplots()
-    sns.lineplot(data=c_men, x='Year', y='Percentage', label='Cis Men', ax=ax)
-    sns.lineplot(data=c_women, x='Year', y='Percentage', label='Cis Women', ax=ax)
-    sns.lineplot(data=t_men, x='Year', y='Percentage', label='Trans Men', ax=ax, alpha=0.7)
-    sns.lineplot(data=t_women, x='Year', y='Percentage', label='Trans Women', ax=ax)
-    sns.lineplot(data=nb, x='Year', y='Percentage', label='Alternative Gender', ax=ax, linestyle="dashed")
+    fig, ax1 = plt.subplots()
+    sns.lineplot(data=c_men, x='Year', y='Percentage', label='Cis Men', ax=ax1)
+    sns.lineplot(data=c_women, x='Year', y='Percentage', label='Cis Women', ax=ax1)
+    sns.lineplot(data=t_men, x='Year', y='Percentage', label='Trans Men', ax=ax1, alpha=0.7)
+    sns.lineplot(data=t_women, x='Year', y='Percentage', label='Trans Women', ax=ax1)
+    sns.lineplot(data=nb, x='Year', y='Percentage', label='Alternative Gender', ax=ax1, linestyle="dashed")
     plt.xlabel('Year')
     plt.ylabel('Percentage Gender')
     plt.title("Gender Breakdown of People Living with HIV over Time")
@@ -94,20 +94,74 @@ def main():
     unknown['Percentage'] = unknown_perc * 100
     #print(unknown)
 
-    fig, ax = plt.subplots()
-    sns.lineplot(data=ai_an, x='Year', y='Percentage', label='American Indian/Alaskan Native', ax=ax, linestyle="dashed")
-    sns.lineplot(data=asian, x='Year', y='Percentage', label='Asian', ax=ax)
-    sns.lineplot(data=black, x='Year', y='Percentage', label='Black', ax=ax)
-    sns.lineplot(data=latine, x='Year', y='Percentage', label='Latinx', ax=ax)
-    sns.lineplot(data=nh_pi, x='Year', y='Percentage', label='Native Hawaiian/Pacific Islander', ax=ax)
-    sns.lineplot(data=white, x='Year', y='Percentage', label='White', ax=ax)
-    sns.lineplot(data=multi_r, x='Year', y='Percentage', label='Multiracial', ax=ax)
-    sns.lineplot(data=unknown, x='Year', y='Percentage', label='Unknown', ax=ax, linestyle="dotted")
+    fig, ax2 = plt.subplots()
+    sns.lineplot(data=ai_an, x='Year', y='Percentage', label='American Indian/Alaskan Native', ax=ax2, linestyle="dashed")
+    sns.lineplot(data=asian, x='Year', y='Percentage', label='Asian', ax=ax2)
+    sns.lineplot(data=black, x='Year', y='Percentage', label='Black', ax=ax2)
+    sns.lineplot(data=latine, x='Year', y='Percentage', label='Latinx', ax=ax2)
+    sns.lineplot(data=nh_pi, x='Year', y='Percentage', label='Native Hawaiian/Pacific Islander', ax=ax2)
+    sns.lineplot(data=white, x='Year', y='Percentage', label='White', ax=ax2)
+    sns.lineplot(data=multi_r, x='Year', y='Percentage', label='Multiracial', ax=ax2)
+    sns.lineplot(data=unknown, x='Year', y='Percentage', label='Unknown', ax=ax2, linestyle="dotted")
     plt.xlabel('Year')
     plt.ylabel('Percentage Race')
     plt.title("Race Breakdown of People Living with HIV over Time")
     plt.savefig('line_plot_race.png', bbox_inches='tight')
 
+    #Transmission
+    transmission = hiv_data[hiv_data['Category'] == 'Transmission']
+    transmission = r_counts.groupby('Year')['Count'].sum().reset_index()
+    #print(transmission)
+
+    mmsc = hiv_data[hiv_data['Group'] == 'Male-to-male sexual contact (MMSC)'].reset_index()
+    mmsc_perc = mmsc['Count'] / transmission['Count']
+    mmsc['Percentage'] = mmsc_perc * 100
+    #print(mtmsc)
+
+    idu = hiv_data[hiv_data['Group'] == 'Injection drug use (IDU)'].reset_index()
+    idu_perc = idu['Count'] / transmission['Count']
+    idu['Percentage'] = idu_perc * 100
+
+    mmsc_idu = hiv_data[hiv_data['Group'] == 'MMSC and IDU'].reset_index()
+    mmsc_idu_perc = mmsc_idu['Count'] / transmission['Count']
+    mmsc_idu['Percentage'] = mmsc_idu_perc * 100
+
+    hrh = hiv_data[hiv_data['Group'] == 'High-risk heterosexual contact (HRH)**'].reset_index()
+    hrh_perc = hrh['Count'] / transmission['Count']
+    hrh['Percentage'] = hrh_perc * 100
+
+    het = hiv_data[hiv_data['Group'] == 'Heterosexual contact (Non-HRH)***'].reset_index()
+    het_perc = het['Count'] / transmission['Count']
+    het['Percentage'] = het_perc * 100
+
+    perinatal = hiv_data[hiv_data['Group'] == 'Perinatal'].reset_index()
+    perinatal_perc = perinatal['Count'] / transmission['Count']
+    perinatal['Percentage'] = perinatal_perc * 100
+
+    unknown_risk = hiv_data[hiv_data['Group'] == 'Unknown risk'].reset_index()
+    unknown_risk_perc = unknown_risk['Count'] / transmission['Count']
+    unknown_risk['Percentage'] = unknown_risk_perc * 100
+
+    other = hiv_data[hiv_data['Group'] == 'Other****'].reset_index()
+    other_perc = other['Count'] / transmission['Count']
+    other['Percentage'] = other_perc * 100
+
+    #all_transmission = pd.concat([mmsc, idu, mmsc_idu, hrh, het, perinatal, unknown_risk, other]).reset_index(drop=True)
+    #print(all_transmission)
+
+    fig, ax3 = plt.subplots()
+    sns.lineplot(data=mmsc, x='Year', y='Percentage', label='Male-Male Sexual Contact (MMSC)', ax=ax3)
+    sns.lineplot(data=idu, x='Year', y='Percentage', label='Injection Drug Use (IDU)', ax=ax3)
+    sns.lineplot(data=mmsc_idu, x='Year', y='Percentage', label='MMSC and IDU', ax=ax3)
+    sns.lineplot(data=hrh, x='Year', y='Percentage', label='High-Risk Heterosexual Contact', ax=ax3)
+    sns.lineplot(data=het, x='Year', y='Percentage', label='Heterosexual Contact (non high-risk)', ax=ax3)
+    sns.lineplot(data=perinatal, x='Year', y='Percentage', label='Perinatal', ax=ax3)
+    sns.lineplot(data=unknown_risk, x='Year', y='Percentage', label='Unknown Risk', ax=ax3)
+    sns.lineplot(data=other, x='Year', y='Percentage', label='Other', ax=ax3)
+    plt.xlabel('Year')
+    plt.ylabel('Percentage Transmissino')
+    plt.title("Transmission Breakdown of People Living with HIV over Time")
+    plt.savefig('line_plot_transmission.png', bbox_inches='tight')
 
 if __name__ == '__main__':
     main()
